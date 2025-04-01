b2aa10bda76a85fc4986810ab6ad23db1b818673
havul <mhaville@gmail.com>
2025-04-01 14:01:21 +0100

    Reset Backend

--- medusa-config.ts
+++ medusa-config.ts
@@ -11,31 +11,6 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-      /* redis_url: REDIS URL, */
     }
   }
 })
-
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_DATABASE = process.env.DB_DATABASE
-
-const DATABASE_URL = 
-  `postgres://${DB_USERNAME}:${DB_PASSWORD}` + 
-  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
-
-  module.exports = {
-    projectConfig: {
-      // ...
-      database_extra: { ssl: { rejectUnauthorized: false } },
-    },
-  }
-
-  module.exports = {
-    projectConfig: {
-      // ...
-      redis_url: process.env.REDIS_URL,
-    },
-  }  
